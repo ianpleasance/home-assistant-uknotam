8633679b0c9b6e01a822cbb67dd6384d59eac42e
Ian Pleasance <ian@planetbuilders.co.uk>
2026-03-17 18:34:28 +0000

    Fix dtutil import

--- custom_components/uknotam/sensor.py
+++ custom_components/uknotam/sensor.py
@@ -7,7 +7,7 @@ from typing import Any
 from homeassistant.components.sensor import SensorEntity, SensorStateClass
 from homeassistant.config_entries import ConfigEntry
 from homeassistant.core import HomeAssistant, callback
-from homeassistant.helpers import dt as dt_util
+from homeassistant.util import dt as dt_util
 from homeassistant.helpers.entity import DeviceEntryType, DeviceInfo
 from homeassistant.helpers.entity_platform import AddEntitiesCallback
 from homeassistant.helpers.update_coordinator import CoordinatorEntity
